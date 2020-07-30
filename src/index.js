e92e142b078a997f3904cf8a63e25520a988738b
Sam Ragucci <samr742@gmail.com>
2020-07-30 11:04:08 -0700

    level 0 working

--- src/index.js
+++ src/index.js
@@ -3,13 +3,27 @@ import {transform} from './travesty.js';
 const TRACE = true;
 const DEBUG = true;
 
+let transformBtn;
+
 if (TRACE) console.log('index.js loaded');
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  if (TRACE) console.log('DOMContentLoaded'); 
+  if (TRACE) console.log('DOMContentLoaded');
+  transformBtn = document.getElementById("transform");
+  transformBtn.addEventListener('click', handleTransform);
 });
 
-// when user clicks Process...
-  
+// when user clicks transform...
+function handleTransform() {
+  const str = document.getElementById("inputtext").value;
+  const newstr = transform(str, 0);
+  updateOutput(newstr);
+}
+
+function updateOutput(str) {
+  const outputtext = document.getElementById("outputtext");
+  outputtext.value = str;
+}
+
 
 // @codekit-append "travesty.js";
--- src/travesty.js
+++ src/travesty.js
@@ -10,13 +10,30 @@ export function log(str) {
 
 export function transform(str, order) {
   let newstr = '';
-  const arr = process(str);
+  // const arr = process(str);
+  if (order === 0) {
+    const arr = CHARS.slice(0);
+    newstr = level0(arr, str);
+    return newstr;
+  }
   
   return str;
 }
 
-export function process(str) {
-  let arr = [];
+// export function process(str) {
+//   let arr = [];
+// 
+//   return arr;
+// }
+
+function level0(arr, str) {
+  const len = str.length;
+  let newstr = '';
+  for (let i=0; i<len; i++) {
+    const n = Math.floor(arr.length * Math.random());
+    const ch = arr[n];
+    newstr += ch;
+  }
   
-  return arr;
+  return newstr;
 }
