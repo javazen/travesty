006450c6cb499553f735ab517c843aea600e71d1
Sam Ragucci <samr742@gmail.com>
2020-08-30 13:24:48 -0700

    Add incr and decr buttons

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import {transform} from './travesty.js';
 
 const TRACE = true;
-// const DEBUG = true;
 
-let levelInput, levelValue = 2, transformBtn;
+let levelInput, levelValue = 2, decrementBtn, incrementBtn, transformBtn;
 
 if (TRACE) console.log('index.js loaded');
 
 document.addEventListener("DOMContentLoaded", function(event) {
   if (TRACE) console.log('DOMContentLoaded');
+  decrementBtn = document.getElementById("decrement");
+  decrementBtn.addEventListener('click', handleDecrement);
+  incrementBtn = document.getElementById("increment");
+  incrementBtn.addEventListener('click', handleIncrement);
   transformBtn = document.getElementById("transform");
   transformBtn.addEventListener('click', handleTransform);
   levelInput = document.getElementById('level');
@@ -18,8 +21,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 // when user changes level...
 function handleLevelInput() {
-  // console.log('input level set to ' + this.value);
   levelValue = +this.value;
+}
+
+// when user clicks decrement...
+function handleDecrement() {
+  levelValue -= 1;
+  const levelText = document.getElementById("level");
+  levelText.value = levelValue;
+  const str = document.getElementById("inputtext").value;
+  const newstr = transform(str, levelValue);
+  updateOutput(newstr);
+}
+
+// when user clicks increment...
+function handleIncrement() {
+  levelValue += 1;
+  const levelText = document.getElementById("level");
+  levelText.value = levelValue;
+  const str = document.getElementById("inputtext").value;
+  const newstr = transform(str, levelValue);
+  updateOutput(newstr);
 }
 
 // when user clicks transform...
